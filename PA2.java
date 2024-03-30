4144b6f3bbb14a8113a84a60f47b7b6b20e93d78
Lauren Martinez <mwa36@txstate.edu>
2024-03-29 22:39:56 -0500

    Added schedEvent method and completed initial Init

--- PA2.java
+++ PA2.java
@@ -11,12 +11,18 @@ public class PA2 {
         System.out.println("Hellow, World!");
     }
 
+    // ARR event = 0
+    // DEP event = 1
+
     public void Init(){
         clock = 0;
         serverIdle = true;
         readyQueueCount = 0;
         eq = new PriorityQueue<>();
+        schedEvent(0, 1, eq); // First event to add. 1 is default time for now
+    }
 
-
+    public void schedEvent(int type, double t, PriorityQueue<Event> e) {
+        e.add(new Event(type, t));
     }
 }
