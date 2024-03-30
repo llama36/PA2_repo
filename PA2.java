d84d3fd135e2d7f1faa99ca8f6dd0a0b36b83c21
Lauren Martinez <mwa36@txstate.edu>
2024-03-29 21:24:35 -0500

    Adding initial values

--- PA2.java
+++ PA2.java
@@ -1,5 +1,18 @@
 public class PA2 {
+
+    private double clock;
+    private boolean serverIdle;
+    private int readyQueueCount;
+
     public static void main(String[] args){
         System.out.println("Hellow, World!");
+    }
+
+    public void Init(){
+        clock = 0;
+        serverIdle = true;
+        readyQueueCount = 0;
+        
+
     }
 }
