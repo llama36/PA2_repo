7fb86481e07f55d63cda6ac025c13fe7be57a262
Lauren Martinez <mwa36@txstate.edu>
2024-03-29 21:31:46 -0500

    Added Event class to repo

---
+++ Event.java
@@ -0,0 +1,19 @@
+public class Event {
+    
+    private int type;
+    private double time;
+
+    public Event() {
+        type = 0;
+        time = 0;
+    }
+
+    void setType(int inputType){
+        type = inputType;
+    }
+
+    void setTime(int inputTime){
+        time = inputTime;
+    }
+    
+}
