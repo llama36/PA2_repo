537d9801d890be1f5fd4443a655da1df5be8d1dc
Lauren Martinez <mwa36@txstate.edu>
2024-03-30 21:05:19 -0500

    Added and initialized avgArrRate and avgServRate

--- PA2.java
+++ PA2.java
@@ -4,15 +4,20 @@ import java.lang.Math;
 
 public class PA2 {
 
-    private static int processCount = 0;
+    private static int processCount;
     private static double clock;
     private static boolean serverIdle;
     private static int readyQueueCount;
+    private static int avgArrRate;
+    private static double avgServRate;
     private static PriorityQueue<EventNode> eq;
     private static Comparator<Double> comparator;
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println();
+
+        avgArrRate = Integer.parseInt(args[0]);
+        avgServRate = Double.parseDouble(args[1]);
 
         Init();
         Run();
@@ -27,27 +32,27 @@ public class PA2 {
         serverIdle = true;
         readyQueueCount = 0;
         eq = new PriorityQueue<>();
-        //comparator = new // WORK ON THIS
         double t = 0;
         t += Math.random();
+        processCount = 1;
         schedEvent(0, t, eq); // First EventNode to add. 1 is default time for now
     }
 
     public static void Run() {
         double timeElapsed = 0;
         double oldClock = 0;
-        //while (processCount <= 10) {
+        while (processCount <= 10) {
             EventNode running = eq.peek();
             oldClock = clock;
             clock = running.getTime();
             timeElapsed = clock - oldClock;
 
             System.out.println("Current process: " + processCount);
-            System.out.print("Time elapsed: " + timeElapsed);
+            System.out.println("Time elapsed: " + timeElapsed);
             System.out.println();
 
             processCount++;
-        //}
+        }
     }
 
     public static void schedEvent(int type, double t, PriorityQueue<EventNode> e) {
